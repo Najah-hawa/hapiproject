b633dc6109252e44d0224b22f862b085800d307d
Najah-hawa <najah.hawa@email.com>
2025-11-25 12:42:20 +0300

    connection to database med MongoDb

---
+++ server.js
@@ -0,0 +1,33 @@
+'use strict';
+
+const Hapi = require('@hapi/hapi');
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+const init = async () => {
+
+    const server = Hapi.server({
+        port: 5000,
+        host: 'localhost'
+    });
+
+     //connect to Mongodb
+    mongoose.connect(process.env.DATABASE).then(() => {
+        console.log("Coonected to MongoDb");
+    }).catch((error) => {
+        console.error("Error connected to database:" + error);
+    });
+    
+
+    // Start the server
+    await server.start();
+    console.log('Server running at:', server.info.uri);
+};
+
+process.on('unhandledRejection', (err) => {
+
+    console.log(err);
+    process.exit(1);
+});
+
+init();
